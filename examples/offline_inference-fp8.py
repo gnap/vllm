1b0f2ebfcf721e19d4abb0a9edfdaece6838591d
George Ang <g@zhihu.com>
2024-06-11 18:45:27 +0800

    add minicpm demo

--- examples/offline_inference-fp8.py
+++ examples/offline_inference-fp8.py
@@ -2,17 +2,18 @@ from vllm import LLM, SamplingParams
 
 # Sample prompts.
 prompts = [
-#    "Hello, my name is",
-    "The president of the United States is" * 100,
-#    "The capital of France is",
-#    "The future of AI is",
+    #    "Hello, my name is",
+    "The president of the United States is" * 1000,
+    #    "The capital of France is",
+    #    "The future of AI is",
 ]
 # Create a sampling params object.
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=1)
 
 # Create an LLM.
-#llm = LLM(model="neuralmagic/Meta-Llama-3-8B-Instruct-FP8", trust_remote_code=True)
+# llm = LLM(model="neuralmagic/Meta-Llama-3-8B-Instruct-FP8", trust_remote_code=True)
 llm = LLM(model="/home/gnap/Models/Meta-Llama-3-8B-Instruct-FP8", trust_remote_code=True)
+# llm = LLM(model="casperhansen/llama-3-8b-instruct-awq", trust_remote_code=True)
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 outputs = llm.generate(prompts, sampling_params)
@@ -28,7 +29,10 @@ start = time.time()
 outputs = llm.generate(prompts, sampling_params)
 for output in outputs:
     prompt = output.prompt
+    metrics = output.metrics
     generated_text = output.outputs[0].text
+    print(len(output.prompt_token_ids))
+    print(metrics.first_token_time - metrics.arrival_time)
     print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
 
 print(time.time() - start)
